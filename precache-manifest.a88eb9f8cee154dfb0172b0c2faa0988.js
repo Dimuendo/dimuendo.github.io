4b523c45b486a63139a47f7b95259133478be36d
Andrew Pak <andrew.pak98@gmail.com>
2020-06-18 21:35:26 -0400

    Updates

---
+++ precache-manifest.a88eb9f8cee154dfb0172b0c2faa0988.js
@@ -0,0 +1,230 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "283558b60bf65d10b5afe88c6e937ee1",
+    "url": "/tftstats/index.html"
+  },
+  {
+    "revision": "e88344a65615393f6101",
+    "url": "/tftstats/static/css/main.7c67a3b3.chunk.css"
+  },
+  {
+    "revision": "30d35ec9bcd6f9c21702",
+    "url": "/tftstats/static/js/2.a8ceecc1.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/tftstats/static/js/2.a8ceecc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e88344a65615393f6101",
+    "url": "/tftstats/static/js/main.ae84fe6a.chunk.js"
+  },
+  {
+    "revision": "25b9c5c0bcaaa8d654e6",
+    "url": "/tftstats/static/js/runtime-main.3310fbf8.js"
+  },
+  {
+    "revision": "32b4bfe18d87be5474541b0c923f4b39",
+    "url": "/tftstats/static/media/38.32b4bfe1.png"
+  },
+  {
+    "revision": "1fd0f160b2119eca1f16c97b1bfbb094",
+    "url": "/tftstats/static/media/ahri.1fd0f160.png"
+  },
+  {
+    "revision": "11595b05556e6308f94eeb2b764a75e0",
+    "url": "/tftstats/static/media/annie.11595b05.png"
+  },
+  {
+    "revision": "0c8fba8b36c427accbb76e6c37b124f0",
+    "url": "/tftstats/static/media/aurelionsol.0c8fba8b.png"
+  },
+  {
+    "revision": "d06d74a274eecebf7e82a41152ebe5e0",
+    "url": "/tftstats/static/media/bard.d06d74a2.png"
+  },
+  {
+    "revision": "e89203ae864e6dccf526e03abb258bc1",
+    "url": "/tftstats/static/media/caitlyn.e89203ae.png"
+  },
+  {
+    "revision": "f4a963ac1c04b017b384103346563a0c",
+    "url": "/tftstats/static/media/cassiopeia.f4a963ac.png"
+  },
+  {
+    "revision": "8f195b303f9e2c62c188e5cdae6daace",
+    "url": "/tftstats/static/media/darius.8f195b30.png"
+  },
+  {
+    "revision": "2419a3e8b0dae8d3c4dbbf0264f7641f",
+    "url": "/tftstats/static/media/ekko.2419a3e8.png"
+  },
+  {
+    "revision": "a719fecd9db5f5d00d0cbbf24a20e0ae",
+    "url": "/tftstats/static/media/ezreal.a719fecd.png"
+  },
+  {
+    "revision": "e40a330fcfa9cf869884707aa1e244f0",
+    "url": "/tftstats/static/media/fiora.e40a330f.png"
+  },
+  {
+    "revision": "6d392aa2d00252bb9f097e57ee15df61",
+    "url": "/tftstats/static/media/fizz.6d392aa2.png"
+  },
+  {
+    "revision": "d2672fd2824fb03b7094f5a6df740e8e",
+    "url": "/tftstats/static/media/gangplank.d2672fd2.png"
+  },
+  {
+    "revision": "25c5c8178c18f7b8035179d6d8670004",
+    "url": "/tftstats/static/media/gnar.25c5c817.png"
+  },
+  {
+    "revision": "87fc11af1ea73506e509c06393eae909",
+    "url": "/tftstats/static/media/graves.87fc11af.png"
+  },
+  {
+    "revision": "51beb898d558a057cbf6526e37099004",
+    "url": "/tftstats/static/media/illaoi.51beb898.png"
+  },
+  {
+    "revision": "796a8d970aff33e814ed1eaca0a042fc",
+    "url": "/tftstats/static/media/irelia.796a8d97.png"
+  },
+  {
+    "revision": "a0d87e1cf6c31371b1642b069b16ab34",
+    "url": "/tftstats/static/media/janna.a0d87e1c.png"
+  },
+  {
+    "revision": "6c87dbd69543bbdcb66576c54051cf83",
+    "url": "/tftstats/static/media/jarvaniv.6c87dbd6.png"
+  },
+  {
+    "revision": "2dc771314b7d5eafda942d28b198259e",
+    "url": "/tftstats/static/media/jayce.2dc77131.png"
+  },
+  {
+    "revision": "23b032db38d7a72467d91eaff7bda714",
+    "url": "/tftstats/static/media/jinx.23b032db.png"
+  },
+  {
+    "revision": "baf4feb722632e1620406f5846075e66",
+    "url": "/tftstats/static/media/karma.baf4feb7.png"
+  },
+  {
+    "revision": "b54bcc968fc1591bc6120f76075301fd",
+    "url": "/tftstats/static/media/kogmaw.b54bcc96.png"
+  },
+  {
+    "revision": "8ef7fab63d4652fd8b281c6c69bc0788",
+    "url": "/tftstats/static/media/leona.8ef7fab6.png"
+  },
+  {
+    "revision": "e4df5fbfc3858baedb54b9295f8ff8e8",
+    "url": "/tftstats/static/media/lucian.e4df5fbf.png"
+  },
+  {
+    "revision": "7f855d79ab30d6c71f6fe447d75a58df",
+    "url": "/tftstats/static/media/malphite.7f855d79.png"
+  },
+  {
+    "revision": "d934df7983c9e0e9df7c9fc555af0352",
+    "url": "/tftstats/static/media/masteryi.d934df79.png"
+  },
+  {
+    "revision": "d04a91a513d577864676060caa987519",
+    "url": "/tftstats/static/media/mordekaiser.d04a91a5.png"
+  },
+  {
+    "revision": "af4a8742e73a931058b8bbda15870fae",
+    "url": "/tftstats/static/media/nautilus.af4a8742.png"
+  },
+  {
+    "revision": "e2b8de5150e08107ad12aea31afca97e",
+    "url": "/tftstats/static/media/neeko.e2b8de51.png"
+  },
+  {
+    "revision": "38def24415974a448a9f637503eaec38",
+    "url": "/tftstats/static/media/nocturne.38def244.png"
+  },
+  {
+    "revision": "7fd78f5dd7fa2b902476070fcd218e57",
+    "url": "/tftstats/static/media/poppy.7fd78f5d.png"
+  },
+  {
+    "revision": "07f905268898d49f385750b11e17641e",
+    "url": "/tftstats/static/media/rakan.07f90526.png"
+  },
+  {
+    "revision": "f49323477cf0ab1ae5d271b33dac13e6",
+    "url": "/tftstats/static/media/riven.f4932347.png"
+  },
+  {
+    "revision": "dfaf533262c017189f3bb5b30773404d",
+    "url": "/tftstats/static/media/rumble.dfaf5332.png"
+  },
+  {
+    "revision": "66e53632729eb18b30eb35a5d3ed6687",
+    "url": "/tftstats/static/media/shaco.66e53632.png"
+  },
+  {
+    "revision": "2e7c2a17b70378982dafa772617ec543",
+    "url": "/tftstats/static/media/soraka.2e7c2a17.png"
+  },
+  {
+    "revision": "c188d959999a0352b1e1f1bafd648f58",
+    "url": "/tftstats/static/media/syndra.c188d959.png"
+  },
+  {
+    "revision": "1249acb45601c7ee6f05473f76162aec",
+    "url": "/tftstats/static/media/teemo.1249acb4.png"
+  },
+  {
+    "revision": "a4b155f412bba91955099ddc816c6e1f",
+    "url": "/tftstats/static/media/thresh.a4b155f4.png"
+  },
+  {
+    "revision": "e591b71a7b77d53b00618df7a0e19538",
+    "url": "/tftstats/static/media/urgot.e591b71a.png"
+  },
+  {
+    "revision": "eef63c80851e40b017a64ec4d2aa13eb",
+    "url": "/tftstats/static/media/vayne.eef63c80.png"
+  },
+  {
+    "revision": "c870dd8b684965a980d4a4b2c584822d",
+    "url": "/tftstats/static/media/vi.c870dd8b.png"
+  },
+  {
+    "revision": "3f0a2617bc6ad49741534321e6cb7362",
+    "url": "/tftstats/static/media/viktor.3f0a2617.png"
+  },
+  {
+    "revision": "6fee0975ab58ac60f25f8db2cd2f3f63",
+    "url": "/tftstats/static/media/xayah.6fee0975.png"
+  },
+  {
+    "revision": "0f7e2b1d695f0c546af1ade7d7b5b5e9",
+    "url": "/tftstats/static/media/xerath.0f7e2b1d.png"
+  },
+  {
+    "revision": "0546ede2047074f80e22b2108b21c9f3",
+    "url": "/tftstats/static/media/xinzhao.0546ede2.png"
+  },
+  {
+    "revision": "8b3c5ce142a3b1360237ac0d12c49b08",
+    "url": "/tftstats/static/media/yasuo.8b3c5ce1.png"
+  },
+  {
+    "revision": "fb9f96ca1f564dad2cfa1927032f2227",
+    "url": "/tftstats/static/media/zed.fb9f96ca.png"
+  },
+  {
+    "revision": "163b85d432fa1d4305bee1cc845d4b9b",
+    "url": "/tftstats/static/media/ziggs.163b85d4.png"
+  },
+  {
+    "revision": "d723458ae7cc9253dfac3852c013bb2e",
+    "url": "/tftstats/static/media/zoe.d723458a.png"
+  }
+]);
